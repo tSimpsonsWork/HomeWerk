e6e81436a84f075ab44eb38e251c4105c6c8e4c1
Trevor Simpson <Tsimpson2994@gmail.com>
2026-08-22 11:48:30 -0500

    First message

---
+++ src/main/java/com/homewerk/backend/HomeWerkApplication.java
@@ -0,0 +1,13 @@
+package com.homewerk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWerkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWerkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homewerk/backend/HomeWerkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homewerk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWerkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
